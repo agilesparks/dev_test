f3f38bc860584e3e97114af86f0238aadaaaeb5b
giladd <giladd@trainologic.com>
2019-04-18 14:37:40 +0300

    add console notice

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+console.error("Please don't use the console! Thanks :)")
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
