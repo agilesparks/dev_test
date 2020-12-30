24d8462e3b1f5672556dec3886e00e223556b97e
giladd <giladd@trainologic.com>
2020-12-30 18:17:21 +0200

    Add copy button for answers

--- src/pages/SubmitError/index.jsx
+++ src/pages/SubmitError/index.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import Header from '../../components/Header';
-import PageContainer from '../../components/PageContainer';
+import React from "react";
+import styled from "styled-components";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Header from "../../components/Header";
+import PageContainer from "../../components/PageContainer";
 
-export default function SubmitErrorPage({data, title}) {
+const Frame = styled.div`
+  border: 1px solid silver;
+  padding: 0.5em;
+  margin-top: 1em;
+  display: inline-block;
+  margin-right: 1em;
+  vertical-align: bottom;
+  border-radius: 5px;
+`;
+
+export default function SubmitErrorPage({ data, title }) {
+  const [copied, setCopied] = React.useState(false);
   return (
     <PageContainer>
-      <Header>
-        {title}
-      </Header>
-      There was an error submitting, sorry for that<br/>
-      Please send your answers by mail:<br/>
-      {data.toString()}
+      <Header>{title}</Header>
+      There was an error submitting, sorry for that
+      <br />
+      Please send your answers by mail:
+      <br />
+      <Frame>{data.toString()}</Frame>
+      <CopyToClipboard
+        text={data.toString()}
+        onCopy={() => setCopied(true)}
+      >
+        <button>{copied ? "Copied" : "Copy Answers"}</button>
+      </CopyToClipboard>
     </PageContainer>
-  )
+  );
 }
